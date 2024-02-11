694b36a2b086becce30ac1df038690739c4d0695
CenkerAydin <20210808002@ogr.akdeniz.edu.tr>
2024-02-11 15:38:42 +0300

    hello kotlin <3

--- Kotlin/HelloWorld/src/main/kotlin/Main.kt
+++ Kotlin/HelloWorld/src/main/kotlin/Main.kt
@@ -1,3 +1,15 @@
-fun main(args: Array<String>) {
+fun main() {
+    //Hello World
+    println("Hello World")
+
+    //Types
+    val name: String="Cenker"
+    val age:Int=22
+    //age =23 -> Error. val cannot reassigned.
+    val surname: String
+    surname= readLine()!!
+
+    var isMale:Boolean =true
 
 }
+
