61063e7e0fd8f13b3b37a4442cf5ac90e32076a4
CenkerAydin <20210808002@ogr.akdeniz.edu.tr>
2024-02-22 20:50:29 +0300

    added Boolean.kt

---
+++ Kotlin/Kotlin_Course/src/main/kotlin/basics/Boolean.kt
@@ -0,0 +1,39 @@
+package basics
+
+fun main() {
+    val isStudent: Boolean =true
+    val isTeacher: Boolean =false
+    val isMale: Boolean?=null
+
+    if (isStudent && isTeacher){
+
+    }
+    if (isStudent and isTeacher){
+
+    }
+    if (isStudent.and(isTeacher)){
+
+    }
+    if (isStudent || isTeacher){
+
+    }
+    if (isStudent or isTeacher){
+
+    }
+    if (isStudent.or(isTeacher)) {
+
+    }
+    if (isStudent.not()){
+
+    }
+    if (!isTeacher){
+
+    }
+
+    if (isStudent == true){
+
+    }
+    if (isStudent){
+
+    }
+}
