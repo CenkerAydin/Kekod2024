2e0c13c0be86e6e393a72c791b3870c34d0e9956
CenkerAydin <20210808002@ogr.akdeniz.edu.tr>
2024-04-15 11:36:53 +0300

    add Encapsulation.kt,

---
+++ Kotlin/Kotlin_Course/src/main/kotlin/classes/Encapsulation.kt
@@ -0,0 +1,10 @@
+package classes
+
+class Encapsulation{
+    val name: String= "Cenker"
+    val surName: String= "Aydın"
+
+    fun getFullName(): String{
+        return "$name $surName"
+    }
+}
